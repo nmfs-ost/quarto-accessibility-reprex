4a27f18e5cef8187d27ab61ad9b0a310977c29e9
Schiano-NOAA <samantha.schiano@noaa.gov>
2025-03-10 11:59:33 -0400

    comment out code for generating initial items: do not use for reprex

--- run_ex.R
+++ run_ex.R
@@ -1,25 +1,25 @@
 # Accessibility Feature to Quarto
 
 # make rda files for a11y use
-asar::convert_output(output_file = "Report.sso",
-                     outdir = file.path(getwd(), "data"),
-                     model = "ss3",
-                     file_save = TRUE,
-                     savedir = file.path(getwd(), "data"),
-                     save_name = "petrale_convert_output"
-                     )
-output <- read.csv(file.path(getwd(), "data", "petrale_convert_output.csv"))
-stockplotr::exp_all_figs_tables(
-  dat = output,
-  ref_line = "msy",
-  ref_line_sb = "msy",
-  indices_unit_label = ""
-  )
-# make figures doc to copy into reprex
-asar::create_figures_doc(
-  subdir = getwd(),
-  rda_dir = getwd()
-)
+# asar::convert_output(output_file = "Report.sso",
+#                      outdir = file.path(getwd(), "data"),
+#                      model = "ss3",
+#                      file_save = TRUE,
+#                      savedir = file.path(getwd(), "data"),
+#                      save_name = "petrale_convert_output"
+#                      )
+# output <- read.csv(file.path(getwd(), "data", "petrale_convert_output.csv"))
+# stockplotr::exp_all_figs_tables(
+#   dat = output,
+#   ref_line = "msy",
+#   ref_line_sb = "msy",
+#   indices_unit_label = ""
+#   )
+# # make figures doc to copy into reprex
+# asar::create_figures_doc(
+#   subdir = getwd(),
+#   rda_dir = getwd()
+# )
 
 # Reproducible example
 quarto::quarto_render("accessibility_repex.qmd")
